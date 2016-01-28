80e3937ac9f61b2c8dbe943571bcbd5eaf5df4f7
valdi1212 <valdi1212@gmail.com>
2016-01-28 23:18:25 +0000

    Uploading work done at home -- est. prog. ~90%

--- PlayingCards.py
+++ PlayingCards.py
@@ -4,7 +4,7 @@ from src.CardPlayer import CardPlayer
 # setting up pygame
 pygame.init()
 window_size = window_width, window_height = 960, 720
-window = pygame.display.set_mode(window_size)
+window = pygame.display.set_mode(window_size, 0, 32)
 pygame.display.set_caption("Card game")
 
 # creating constants
@@ -15,18 +15,27 @@ FOREST_GREEN = (34, 139, 34)
 
 window.fill(FOREST_GREEN)
 
-# creating bool var and player objects
+# creating variables
 running = True
 clicked = False
-# TODO: make CardPlayer class actually usable
+timer = pygame.time.Clock()
+player = CardPlayer(200, 600, 1, window)
+advers1 = CardPlayer(0, 50, 2, window)
+advers2 = CardPlayer(200, 0, 1, window)
+advers3 = CardPlayer(860, 50, 2, window)
 
 while running:
-    # TODO: Render the cards
+    player.draw_hand()
+    advers1.draw_hand()
+    advers2.draw_hand()
+    advers3.draw_hand()
+    # TODO: make the game only draw hands after the mouse is clicked
     event = pygame.event.poll()
     if event.type == pygame.QUIT:
         running = False
     # elif event.type == pygame.MOUSEBUTTONDOWN and event.button == LEFT_BUTTON:
         # TODO: call functions that would display the cards
+        # TODO call function that would calculate who has highest sum and declare the winner
 
     pygame.display.update()
 
--- src/CardPlayer.py
+++ src/CardPlayer.py
@@ -3,10 +3,51 @@ import random
 
 pygame.init()
 
+# creating constants
+FRAME_WIDTH = 81
+FRAME_HEIGHT = 117
+
+# loading in the sprites
+# TODO: load the face down card image from the sheet
+card_frames = []
+
+image = pygame.image.load("src/sprites/cards.gif")
+sheet_width, sheet_height = image.get_size()
+
+for row in range(0, 4):
+    for col in range(int(sheet_width / FRAME_WIDTH)):
+        card_frames.append(image.subsurface((col * FRAME_WIDTH, row * FRAME_HEIGHT, FRAME_WIDTH, FRAME_HEIGHT)))
+
+number_of_sprites = len(card_frames)
+print(str(number_of_sprites))
+
 
 class CardPlayer:
 
-    def __init__(self, x, y, orientation):
+    def __init__(self, x, y, orientation, window):
         self.x = x
         self.y = y
         self.orientation = orientation
+        self.window = window
+
+        self.hand = []
+        self.card_draw()
+
+    def card_draw(self):
+        for i in range(5):
+            self.hand.append(card_frames[random.randint(0, 51)])
+            # TODO: make certain you can't draw a card that's already been drawn
+
+    def draw_hand(self):
+        temp_x = 0
+        temp_y = 0
+        if self.orientation == 1:
+            for card in self.hand:
+                self.window.blit(card, (self.x + temp_x, self.y))
+                temp_x += 101
+        elif self.orientation == 2:
+            for card in self.hand:
+                self.window.blit(card, (self.x, self.y + temp_y))
+                temp_y += 127
+
+    # TODO: draw face down card stacks
